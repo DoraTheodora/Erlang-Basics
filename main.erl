e8a888760ce33d1a49bed7ab05a01b83d6c156b6
DoraTheodora <tataru.theodora@yahoo.com>
2021-01-29 16:52:44 +0000

    concatonate, append and insert DONE

--- main.erl
+++ main.erl
@@ -52,6 +52,36 @@ del(Item, [Item|Tail])->
 del(Item, [Head|Tail])->
     [Head|del(Item, Tail)].
 
+% * insertAtFront() inserts an element at the front of the list
+insertAtFront(Element, [])->
+    [Element];
+insertAtFront(Element,[H|T])->
+    [Element|insertAtFront(H, T)].
+
+% * appendAtEnd() inserts and element at the end of the list
+appendAtEnd(Element, [])->
+    [Element];
+appendAtEnd(Element, [H|T])->
+    [H|appendAtEnd(Element, T)].
+
+concatonateTwoLists([], [])->
+    [];
+concatonateTwoLists([], [H|T])->
+    [H|T];
+concatonateTwoLists([H|T], [])->
+    [H|T];
+concatonateTwoLists(L1,[H|T])->
+    %[[A|concatonateTwoLists(B, [])] | [H|concatonateTwoLists([],T)]].
+    concatonateTwoLists(appendAtEnd(H, L1), T).
+    
+% TODO: insertAtFront(q, [1,2,3]) == [q,1,2,3]
+% TODO: appeend(q, [1,2,3]) == [1,2,3,q]
+% TODO: concatonate ([1,2,3], [a,b,c]) == [1,2,3,a,b,c]
+% TODO: zip([1,2,3].[a,b,c]) == [1,a,2,b,3,c]
+% TODO: flatten([[1,2,[3]], 4,5, [[[6]]]) == [1,2,3,4,5,6]
+
+
+
 % * for(start, end, function)
 for(Start, Start, Function)->
     [];
@@ -60,10 +90,5 @@ for(Start, End, Function)->
     [Function(Start)|for(Start+1, End, Function)].
 
 
-% TODO: insert(q, [1,2,3]) == [q,1,2,3]
-% TODO: appeend(q, [1,2,3]) == [1,2,3,q]
-% TODO: concatonate ([1,2,3], [a,b,c]) == [1,2,3,a,b,c]
-% TODO: zip([1,2,3].[a,b,c]) == [1,a,2,b,3,c]
-% TODO: flatten([[1,2,[3]], 4,5, [[[6]]]) == [1,2,3,4,5,6]
 
 
